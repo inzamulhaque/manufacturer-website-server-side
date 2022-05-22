284d5540ba3ef83a55b6ab3792c6c81b38f9eb14
inzamulhaque <mdihalif@gmail.com>
2022-05-22 23:45:12 +0600

    add jot verify function

--- index.js
+++ index.js
@@ -13,12 +13,29 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ftcfo.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+const verifyJWT = (req, res, next) => {
+    const authHeaders = req.headers.authorization;
+    if (!authHeaders) {
+        return res.status(401).send({ message: "unauthorized access" });
+    }
+
+    const getToken = authHeaders.split(" ")[1];
+
+    jwt.verify(getToken, process.env.JWT_KEY, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: "forbidden access" });
+        }
+        req.decoded = decoded;
+        next();
+    });
+}
+
 async function run() {
     try {
         await client.connect();
         const userCollection = client.db("ih_electronics").collection("users");
 
-
+        // create new user and set default role
         app.post("/user/:email", async (req, res) => {
             const { email } = req.params;
             const user = req.body;
@@ -30,6 +47,11 @@ async function run() {
             }
             const token = jwt.sign({ email }, process.env.TOKEN_KEY, { expiresIn: '1h' });
             res.send({ token });
+        });
+
+        // get one user
+        app.get("/user/:email", verifyJWT, async (req, res) => {
+
         });
     } finally {
 
