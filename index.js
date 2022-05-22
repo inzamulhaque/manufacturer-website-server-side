ab9654ff7b7fcba8d8bbfda5c4c42b13ecd7565c
inzamulhaque <mdihalif@gmail.com>
2022-05-22 20:06:42 +0600

    create user api

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+// contect mongodb
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ftcfo.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const userCollection = client.db("ih_electronics").collection("users");
+
+
+        app.post("/user/:email", async (req, res) => {
+            const { email } = req.params;
+            const user = req.body;
+            const filter = { email };
+
+            const findUser = await userCollection.findOne(filter);
+            if (!findUser) {
+                await userCollection.insertOne(user);
+            }
+            const token = jwt.sign({ email }, process.env.TOKEN_KEY, { expiresIn: '1h' });
+            res.send({ token });
+        });
+    } finally {
+
+    }
+}
+
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
     res.send("SERVER RUNNING");
